bbe4a3781b80e4ec12a8acfa8bec3866874410e1
Tobi Akere <148399418+Tobidevs@users.noreply.github.com>
2025-03-17 22:22:18 -0500

    useContext init

--- Movie app/src/pages/home.jsx
+++ Movie app/src/pages/home.jsx
@@ -27,7 +27,7 @@ function Home() {
     e.preventDefault();
     if (!searchQuery.trim()) return;
     if (loading) return;
-    
+
     setLoading(true);
     try {
       const searchResults = await searchMovies(searchQuery);
